271976484ecbb77d865ee9469a075f84dd6997bd
robopecha <julikolb@gmail.com>
2023-06-20 15:18:07 +0200

    working pages

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+//default
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+//home
+app.get("/home", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+//about
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+//works
+app.get("/works", (request, response, next) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/photos", (request, response, next) => {
+  response.sendFile(__dirname + "/views/photos.html");
+});
+
+//portdisplay
+const port = 3000;
+app.listen(port, () => {
+  console.log(`***Server is running in port : ${port}***`);
+});
