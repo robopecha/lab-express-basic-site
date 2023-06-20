9eedbad33b5334cd7ca4ba69cc1b7d5c7b249e7c
robopecha <julikolb@gmail.com>
2023-06-20 16:45:58 +0200

    solved lab

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+// app.use(express.static("views"));
 
 //default
 app.get("/", (request, response, next) => {
